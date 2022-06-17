f2085844f5f014ee26ca58258eb78567e9b5b49b
Vladyslav Lazurenko <vladyslavl@tabtale.com>
2022-06-17 11:16:43 +0300

    fix error package code

--- libs/errors/src/index.ts
+++ libs/errors/src/index.ts
@@ -2,4 +2,4 @@ import { StatusCodes } from 'http-status-codes'
 
 export const NOT_FOUND_CODE = StatusCodes.NOT_FOUND;
 
-export const INTERNAL_SERVER_ERROR_CODE = StatusCodes.INTERNAL_SERVER_ERROR;
+export const FORBIDDEN_CODE = StatusCodes.FORBIDDEN;
