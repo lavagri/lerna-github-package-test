58f2b62658e416afa3de2faca8447a7b688bc84a
Vladyslav Lazurenko <vladyslavl@tabtale.com>
2022-06-17 11:35:35 +0300

    new code in lib

--- libs/errors/src/index.ts
+++ libs/errors/src/index.ts
@@ -3,3 +3,5 @@ import { StatusCodes } from './config'
 export const NOT_FOUND_CODE = StatusCodes.NOT_FOUND;
 
 export const FORBIDDEN_CODE = StatusCodes.FORBIDDEN;
+
+export const UNAUTHORIZED_CODE = StatusCodes.UNAUTHORIZED;
